e8a7b8916d593145a05f1a7307ad90efc85863cd
Franco Naso <francogabrielnaso@gmail.com>
2023-01-27 01:35:58 -0300

    5

---
+++ ddd.js
@@ -0,0 +1,322 @@
+const productos = [
+    {
+        id: "Carro-01",
+        titulo: "Carro-01",
+        imagen: "../img/carro 1.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "1",
+        categoria: {
+            nombre: "carro",
+            id: "carro",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+
+    {
+        id: "Carro-02",
+        titulo: "Carro-02",
+        imagen: "../img/carro2.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "2",
+        categoria: {
+            nombre: "carro",
+            id: "carro",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+
+    {
+        id: "Carro-03",
+        titulo: "Carro-03",
+        imagen: "../img/carro3.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "3",
+        categoria: {
+            nombre: "carro",
+            id: "carro",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Carro-04",
+        titulo: "Carro-04",
+        imagen: "../img/carro4.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "4",
+        categoria: {
+            nombre: "carro",
+            id: "carro",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+
+    //Bancos
+    {
+        id: "Banco-01",
+        titulo: "Banco-01",
+        imagen: "../img/carro5.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "5",
+        categoria: {
+            nombre: "Banco",
+            id: "Banco",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Banco-02",
+        titulo: "Banco-02",
+        imagen: "../img/carro6.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "6",
+        categoria: {
+            nombre: "Banco",
+            id: "Banco",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Banco-03",
+        titulo: "Banco-03",
+        imagen: "../img/carro7.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "7",
+        categoria: {
+            nombre: "banco",
+            id: "banco",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Banco-04",
+        titulo: "Banco-04",
+        imagen: "../img/carro8.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "8",
+        categoria: {
+            nombre: "Banco",
+            id: "Banco",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Banco-05",
+        titulo: "Banco-05",
+        imagen: "../img/carro9.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "9",
+        categoria: {
+            nombre: "Banco",
+            id: "Banco",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    //Tableros
+    {
+        id: "Tablero-01",
+        titulo: "Tablero-01",
+        imagen: "../img/carro10.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "9",
+        categoria: {
+            nombre: "Tablero",
+            id: "Tablero",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Tablero-02",
+        titulo: "Tablero-02",
+        imagen: "../img/carro11.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "10",
+        categoria: {
+            nombre: "Tablero",
+            id: "Tablero",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Tablero-03",
+        titulo: "Tablero-03",
+        imagen: "../img/carro12.png",
+        imgS1: "../img/slider/01.png",
+        imgS2: "../img/slider/06.png",
+        imgS3: "../img/slider/7.png",
+        imgS4: "../img/slider/08.png",
+        dni: "11",
+        categoria: {
+            nombre: "Tablero",
+            id: "Tablero",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+    {
+        id: "Tablero-04",
+        titulo: "Tablero-04",
+        imagen: "../img/carro13.png",
+        dni: "12",
+        categoria: {
+            nombre: "Tablero",
+            id: "Tablero",
+            descripcion: "es un carro muy bonito",
+            ancho: "80cm",
+            alto: "110cm",
+        }
+    },
+]
+
+let list = document.getElementById("list");
+let cerrar = document.getElementById("close")
+let menu = document.querySelector(".header__ul");
+let botonMenu = document.querySelectorAll(".boton-menu");
+
+list.addEventListener("click", ()=> {
+menu.classList.add("show");
+list.classList.add("off");
+cerrar.classList.remove("off")
+
+cerrar.addEventListener("click", ()=>{
+    menu.classList.remove("show");
+    list.classList.remove("off");
+    cerrar.classList.add("off")
+})
+
+})
+
+botonMenu.forEach(boton => {
+    boton.addEventListener("click", () => {
+        menu.classList.remove("show");
+        list.classList.remove("off");
+        cerrar.classList.add("off")
+    })
+})
+
+let ventana = document.querySelector(".ventana")
+let botonesVer = document.querySelectorAll(".producto-ver")
+let contenedorModal = document.getElementById("contenedor_modal")
+let nombre = document.querySelector(".modal_titulo")
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector(".titulo-principal")
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+
+    productosElegidos.forEach(producto => {
+        let div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+        <img  class="producto-img" src="${producto.imagen}" alt="carro">
+        <div class="producto-detalles" >
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <button id="${producto.dni}" class="producto-ver"> <a href="./producto.html">ver mas</a> </button>
+        </div>`;
+        contenedorProductos.append(div);
+    });
+    actualizarBotonesVer()
+
+}
+cargarProductos(productos);
+
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        e.currentTarget.classList.add("activado");
+        if (e.currentTarget.id != "Todo") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+
+            cargarProductos(productosBoton);
+
+        } else {
+            tituloPrincipal.innerText = "Todos los productos"
+            cargarProductos(productos);
+        }
+    })
+})
+
+
+
+
+function actualizarBotonesVer(producto) {
+    botonesVer = document.querySelectorAll(".producto-ver");
+    botonesVer.forEach(botonVer => {
+        botonVer.addEventListener("click", agregarNuevaPag);
+    })
+
+}  
+const productoAbrir = []
+console.log(productoAbrir)
+function agregarNuevaPag (e){
+const idBoton = e.currentTarget.id;
+const productoElegido = productos.find(producto => producto.dni === idBoton)
+productoAbrir.push(productoElegido) 
+localStorage.setItem("producto-elegido", JSON.stringify(productoAbrir));
+}
+
+
--- main .js
+++ main .js
@@ -1,23 +1,42 @@
 const url = "https://docs.google.com/spreadsheets/d/1YdQ32dfBhAPpHqcSiDNQGwBw9rX0DaBZ_2fqFDyvhLs/export?format=csv";
-const main = document.querySelector("main");
+
+const contenedorProductos = document.querySelector("main");
+const botonesCategorias = document.querySelectorAll(".boton-categoria")
+
+
+
 fetch(url).then(result => result.text()).then(function (csvtext) {
     return csv().fromString(csvtext);
 }).then(function (csv) {
-    csv.forEach(function (row) {
-    main.innerHTML += `
-    <div class="carta">
-    <img class="imagen" src="${row.imagen}" alt="">
-    <h1 class="titulo">${row.titulo}</h1>
-    <p class="descripcion">${row.descripcion}</p>
-    <p class="precio">${row.precio}$</p>
 
-</div>
-    
-    `
+    function cargarProductos(productosElegidos) {
+        contenedorProductos.innerHTML = "";
+        productosElegidos.forEach(row => {
+            let div = document.createElement("div");
+            div.classList.add("carta");
+            div.innerHTML = `
+                <img  class="imagen" src="${row.imagen}" alt="carro">
+                <h3 class="titulo">${row.precio}</h3>
+                <p class="descripcion">${row.descripcion}</p>`;
+            contenedorProductos.append(div);
+        });
+
+    }
+    cargarProductos(csv);
+
+    botonesCategorias.forEach(botonCategoria => {
+        botonCategoria.addEventListener("click", (e) => {
+            if (e.currentTarget.id != "todo") {
+                const elegidos = csv.filter(row => row.tipo == e.currentTarget.id);
+                cargarProductos(elegidos);
+            } else {
+                cargarProductos(csv);
+            }
+        })
     })
-});
 
 
 
+})
 
 
